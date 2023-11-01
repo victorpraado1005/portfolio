c5176af5b3dd10c1f4ab8aaf900509d5c28cbf7a
Victor Prado <prado.victor1005@gmail.com>
2023-11-01 17:30:21 -0300

    fix: adicionando projeto petverso no carrousel de projetos pessoais.

--- src/app/components/carouselProjects/index.js
+++ src/app/components/carouselProjects/index.js
@@ -14,6 +14,47 @@ const breakPoints = [
 const itens = [
   {
     id: 1,
+    title: "PetVerso",
+    status: "Concluido",
+    description: "Sistema criado em React para controle de todas as informações do seu animal. Projeto de TCC - FIAP 2022",
+    link_github : "https://github.com/victorpraado1005/fe-petverso",
+      link_projeto : "https://petverso.vercel.app/",
+    tecnologias: [
+      "javascript",
+      "react",
+      "nodejs",
+      "postgres",
+    ]
+  },
+  {
+    id: 2,
+    title: "PetVerso - API",
+    status: "Concluido",
+    description: "API criada em NodeJs com Express para as validações das informações do projeto PetVerso.",
+    link_github : "https://github.com/victorpraado1005/PetVerso",
+    link_projeto : "https://petverso.vercel.app/",
+    tecnologias: [
+      "javascript",
+      "nodejs",
+      "express",
+      "postgres",
+    ]
+  },
+  {
+    id: 3,
+    title: "Site - PetVerso",
+    status: "Concluido",
+    description: "Site desenvolvido para o projeto de TCC do curso de sistema de informação.",
+    link_github: "https://github.com/victorpraado1005/site-PetVerso",
+    link_projeto: "https://site-petverso.vercel.app/",
+    tecnologias: [
+      "html",
+      "css",
+      "javascript"
+    ]
+  },
+  {
+    id: 4,
     title: "Site Takefoods",
     status: "Concluido",
     description: "Site desenvolvido para a empresa Takefoods, 100% responsivo. Utilizando apenas HTML e CSS puro e JS para algumas funcionalidades e Firebase para armazenamento de informações.",
@@ -27,20 +68,7 @@ const itens = [
     ]
   },
   {
-    id: 2,
-    title: "Site - PetVerso",
-    status: "Concluido",
-    description: "Site desenvolvido para o projeto de TCC do curso de sistema de informação.",
-    link_github: "https://github.com/victorpraado1005/site-PetVerso",
-    link_projeto: "https://site-petverso.vercel.app/",
-    tecnologias: [
-      "html",
-      "css",
-      "javascript"
-    ]
-  },
-  {
-    id: 3,
+    id: 5,
     title: "SchoolJS",
     status: "Concluido",
     description: "Projeto desenvolvido para aprimorar minhas habilidades com JS. Sistema de controle de alunos e notas para professores, com opção de criar, editar e excluir notas e alunos.",
@@ -55,7 +83,7 @@ const itens = [
     ]
   },
   {
-    id: 4,
+    id: 6,
     title: "API Center",
     status: "Concluido",
     description: "Projeto criado para consulta de API´s públicas. API de câmbio diário, tempo e consulta de CEP.",
